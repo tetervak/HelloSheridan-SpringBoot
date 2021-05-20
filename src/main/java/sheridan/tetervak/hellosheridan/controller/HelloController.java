79ecf4d05e25c25435d0333222c4015061f83d4b
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-05-20 15:51:22 -0400

    received inputs and displayed outputs

--- src/main/java/sheridan/tetervak/hellosheridan/controller/HelloController.java
+++ src/main/java/sheridan/tetervak/hellosheridan/controller/HelloController.java
@@ -27,7 +27,13 @@ public class HelloController {
     }
 
     @GetMapping("/Output")
-    public String output(){
+    public String output(@RequestParam String firstName,
+                         @RequestParam String lastName,
+                         Model model) {
+
+        model.addAttribute("firstName", firstName);
+        model.addAttribute("lastName", lastName);
+
         return "Output";
     }
 
