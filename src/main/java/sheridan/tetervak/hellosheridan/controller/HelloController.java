3c4dd03663b90ede4987514790429cbd493e4c57
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-05-20 15:16:41 -0400

    used a path variable

--- src/main/java/sheridan/tetervak/hellosheridan/controller/HelloController.java
+++ src/main/java/sheridan/tetervak/hellosheridan/controller/HelloController.java
@@ -1,13 +1,23 @@
 package sheridan.tetervak.hellosheridan.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("/Hello")
-    public String hello(){
+    public String hello1(@RequestParam(defaultValue = "Sheridan") String name, Model model){
+        model.addAttribute("name", name);
+        return "Hello";
+    }
+
+    @GetMapping("/Hello/{name}")
+    public String hello2(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
         return "Hello";
     }
 
