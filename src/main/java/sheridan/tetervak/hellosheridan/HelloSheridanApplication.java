f806b0c31b940c3a7eeec0db602ade3aef5b83df
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-05-20 13:47:49 -0400

    created the empty project

---
+++ src/main/java/sheridan/tetervak/hellosheridan/HelloSheridanApplication.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.hellosheridan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSheridanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloSheridanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sheridan/tetervak/hellosheridan/HelloSheridanApplicationTests.java
@@ -0,0 +1,13 @@
+package sheridan.tetervak.hellosheridan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSheridanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
