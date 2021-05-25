d57e581dc960dcc4bf975acff7d30a8c41bc634e
Alexander Tetervak <tetervak@sheridancollege.ca>
2021-05-25 13:50:34 -0400

    split the controller into two controllers

--- src/main/java/sheridan/tetervak/hellosheridan/controller/HelloController.java
+++ src/main/java/sheridan/tetervak/hellosheridan/controller/HelloController.java
@@ -21,20 +21,6 @@ public class HelloController {
         return "Hello";
     }
 
-    @GetMapping("/Input")
-    public String input(){
-        return "Input";
-    }
 
-    @GetMapping("/Output")
-    public String output(@RequestParam String firstName,
-                         @RequestParam String lastName,
-                         Model model) {
-
-        model.addAttribute("firstName", firstName);
-        model.addAttribute("lastName", lastName);
-
-        return "Output";
-    }
 
 }
